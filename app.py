cf6e50c56a16ef00f97a424218aa8d874c2b0299
RUBÉN GONZÁLEZ VELASCO <rubengv@kth.se>
2024-05-28 12:29:26 +0200

    v0.4

--- app.py
+++ app.py
@@ -359,4 +359,6 @@ def stop_classification():
     return jsonify({'status': 'Classification stopped'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
